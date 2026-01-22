d71a2a28220b3985cee3af7901c251947a8fe8b6
nazarukiv <nazaruk7649@ukr.net>
2026-01-22 17:18:01 +0000

    start

---
+++ src/main/java/com/nazarukiv/springwebblog/SpringWebBlogApplication.java
@@ -0,0 +1,13 @@
+package com.nazarukiv.springwebblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nazarukiv/springwebblog/SpringWebBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nazarukiv.springwebblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
