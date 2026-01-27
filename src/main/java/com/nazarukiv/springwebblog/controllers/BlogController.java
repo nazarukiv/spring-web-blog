532f6dfbfd51a0736a4f7243ae7d8b3a550bfd25
nazarukiv <nazaruk7649@ukr.net>
2026-01-27 14:49:22 +0000

    add a 'in deatils' section to each of the articles

--- src/main/java/com/nazarukiv/springwebblog/controllers/BlogController.java
+++ src/main/java/com/nazarukiv/springwebblog/controllers/BlogController.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -23,6 +27,18 @@ public class BlogController {
     @GetMapping("/blog/add")
     public String blogAdd(Model model){
         return "blog-add";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String blogDetails(@PathVariable long id, Model model) {
+        if (!postRepository.existsById(id)){
+            return "redirect:/blog";
+        }
+
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Post not found"));
+        model.addAttribute("post", post);
+        return "blog-details";
     }
 
 }
